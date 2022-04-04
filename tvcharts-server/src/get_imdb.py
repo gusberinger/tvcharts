1125fc0dee4e63b60c4f4411bbc1c0521e924d3d
Gus Beringer <berin013@umn.edu>
2022-04-04 02:45:38 -0500

    get_series now uses local db

--- tvcharts-server/src/get_imdb.py
+++ tvcharts-server/src/get_imdb.py
@@ -1,23 +1,24 @@
-from typing import Iterable
-from imdb import Cinemagoer
 from collections import namedtuple
+from this import d
+from typing import Iterable
+import sqlalchemy as db
+from dotenv import dotenv_values
+config = dotenv_values(".env")
 
 
-Episode = namedtuple('Episode', ['imdb_id', 'title', 'season', 'episode_number', 'rating'])
-ia = Cinemagoer()
+Episode = namedtuple('Episode', ['tconst', 'title', 'season', 'episode_number', 'rating', 'votes'])
+engine = db.create_engine(f"mysql+pymysql://{config['USERNAME']}:{config['PASSWORD']}@localhost/series")
 
-
-def get_show_data(imdb_id) -> Iterable[Episode]:
-    series = ia.get_movie(imdb_id)
-    ia.update(series, 'episodes')
-    for season in series['episodes']:
-        for episode_number in series['episodes'][season]:
-            episode = series['episodes'][season][episode_number]
-            rating = episode['rating']
-            title = episode['title']
-            yield Episode(imdb_id, title, season, episode_number, rating)
+def get_series(tconst : str) -> Iterable[Episode]:
+    with engine.connect() as connection:
+        results = connection.execute(f"SELECT * FROM data WHERE parentTconst='{tconst}'")
+        rows = results.fetchall()
+    for row in rows:
+        tconst, _, seasonNumber, episodeNumber, primaryTitle, averageRating, numVotes = row
+        yield Episode(tconst, primaryTitle, seasonNumber,
+                      episodeNumber, averageRating, numVotes)
 
 
 if __name__ == "__main__":
-    for episodes in get_show_data('0141842'):
+    for episodes in get_series('tt0141842'):
         print(episodes)
