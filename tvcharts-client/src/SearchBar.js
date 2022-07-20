5633e991ccf2ab6635afb80211b6522530d198fc
Gus Beringer <berin013@umn.edu>
2022-07-20 15:23:13 -0500

    working selection search bar

--- tvcharts-client/src/SearchBar.js
+++ tvcharts-client/src/SearchBar.js
@@ -1,68 +1,56 @@
-import React, { useState } from "react"
+import React, {useState} from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import './SearchBar.css'
 
-const SearchBar = () => {
-  const [searchInput, setSearchInput] = useState("");
-  const countries = [
-    { name: "Belgium", continent: "Europe" },
-    { name: "India", continent: "Asia" },
-    { name: "Bolivia", continent: "South America" },
-    { name: "Ghana", continent: "Africa" },
-    { name: "Japan", continent: "Asia" },
-    { name: "Canada", continent: "North America" },
-    { name: "New Zealand", continent: "Australasia" },
-    { name: "Italy", continent: "Europe" },
-    { name: "South Africa", continent: "Africa" },
-    { name: "China", continent: "Asia" },
-    { name: "Paraguay", continent: "South America" },
-    { name: "Usa", continent: "North America" },
-    { name: "France", continent: "Europe" },
-    { name: "Botswana", continent: "Africa" },
-    { name: "Spain", continent: "Europe" },
-    { name: "Senegal", continent: "Africa" },
-    { name: "Brazil", continent: "South America" },
-    { name: "Denmark", continent: "Europe" },
-    { name: "Mexico", continent: "South America" },
-    { name: "Australia", continent: "Australasia" },
-    { name: "Tanzania", continent: "Africa" },
-    { name: "Bangladesh", continent: "Asia" },
-    { name: "Portugal", continent: "Europe" },
-    { name: "Pakistan", continent: "Asia" },
-  ]
+
+const SearchBar = ({placeholder, data, selectItem}) => {
+  const [filteredData, setFilteredData] = useState([])
+  const [word, setWord] = useState("")
 
   const handleChange = (e) => {
-    e.preventDefault()
-    setSearchInput(e.target.value)
+    const word = e.target.value
+    if (word != "") {
+      const newFilter = data.filter((value) => {
+        return value.title.toLowerCase().includes(word.toLowerCase())
+      })
+      setFilteredData(newFilter)
+    }
+    else {
+      setFilteredData([])
+    }
   }
 
-  if (searchInput.length > 0) {
-    countries.filter((country) => {
-      return country.name.match(searchInput)
-    })
+  const handleSelect = e => {
+    setFilteredData([])
+    setWord("")
+    selectItem(e)
   }
 
   return (
-    <>
-    <input type="search"
-    placeholder="Search TV Show"
-    onChange={handleChange}
-    value={searchInput}/>
-    <table>
-      <tr>
-        <th>Country</th>
-        <th>Continent</th>
-      </tr>
-      {countries.map((country, index) => {
-        <div>
-          <tr>
-            <td>{country.name}</td>
-            <td>{country.continent}</td>
-          </tr>
+    <div className='search'>
+      <div className="searchInputs">
+        <input
+          type="text"
+          placeholder={placeholder}
+          onChange={handleChange}
+        />
+        <div className="searchIcon">
+          <i className="fa-pencil" title="Edit"></i>
+          {/* <FontAwesomeIcon icon="fa-solid fa-magnifying-glass" /> */}
+          {/* {filteredData.length != 0 ? <FontAwesomeIcon icon="fa-solid fa-magnifying-glass" /> : <FontAwesomeIcon icon="fa-solid fa-x" />} */}
         </div>
-      })}
-    </table>
-    </>
+      </div>
+      <div className="dataResult">
+        {filteredData.slice(0, 4).map((value, key) => {
+          return (
+          <a className="dataItem" target="_blank" onClick={handleSelect} id={value.id}>
+            <p tconst={value.id}>{value.title}</p>
+          </a>
+          )
+        })}
+      </div>
+    </div>
   )
 }
-
 
 export default SearchBar
