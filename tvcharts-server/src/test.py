d657cb2beaad7f840523e3911cc17661ed200841
Gus Beringer <berin013@umn.edu>
2022-07-21 17:34:55 -0500

    start of testing script

---
+++ tvcharts-server/src/test.py
@@ -0,0 +1,22 @@
+from tqdm import tqdm
+from server import app, results
+import unittest
+
+class FlaskTestCase(unittest.TestCase):
+
+    def test_sopranos(self):
+        tester = app.test_client(self)
+        response = tester.get("/tconst/tt0141842", content_type = "html/text")
+        self.assertEqual(response.status_code, 200)
+
+    def test_all(self):
+        tconsts = [x[0] for x in results] 
+        for tconst in tqdm(tconsts):
+            tester = app.test_client(self)
+            response = tester.get(f"/tconst/{tconst}", content_type = "html/text")
+            self.assertEqual(response.status_code, 200)
+
+
+if __name__ == "__main__":
+    print(results)
+    unittest.main()
