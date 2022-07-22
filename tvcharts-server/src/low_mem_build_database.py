4746b8e3c7a18ebb2c878eb3ab70577239dd9840
Gus Beringer <gusberinger@gmail.com>
2022-07-22 21:46:59 +0100

    low mem script start

--- tvcharts-server/src/low_mem_build_database.py
+++ tvcharts-server/src/low_mem_build_database.py
@@ -1,3 +1,4 @@
+import chunk
 import gzip
 from numpy import full
 import pandas as pd
@@ -26,15 +27,46 @@ def download_file(url, filename):
         print(f"{filename} Already exists, skipping")
 
 
-
 if __name__ == "__main__":
-    engine = db.create_engine(
-        f"sqlite:///{db_path}"
-    )
+    engine = db.create_engine(f"sqlite:///{db_path}")
 
     with engine.connect() as connection:
         connection.execute("DROP TABLE IF EXISTS data;")
-        connection.execute("DROP TABLE IF EXISTS titles;")
+        connection.execute("DROP TABLE IF EXISTS search;")
 
     if not dump_path.exists():
         dump_path.mkdir()
+
+    download_file(title_ratings_url, "title.ratings.tsv.gz")
+    download_file(title_basics_url, "title.basics.tsv.gz")
+    download_file(title_episodes_url, "title.episodes.tsv.gz")
+
+    
+    # load title.basics in chunks into search table
+    with gzip.open(dump_path.joinpath("title.basics.tsv.gz"), "rb") as fp:
+        with pd.read_csv(
+            fp,
+            sep="\t",
+            usecols=["tconst", "titleType", "primaryTitle", "startYear", "endYear"],
+            na_values="\\N",
+            chunksize=1000,
+        ) as reader:
+            for chunk in reader:
+                series_info = chunk[chunk["titleType"] == "tvSeries"]
+                series_info = series_info.drop(["titleType"], axis=1)
+                series_info.to_sql(
+                    "search", con=engine, if_exists="append", index=False
+                )
+    
+    # load title.episodes into episodes table
+    with gzip.open(dump_path.joinpath("title.episodes.tsv.gz"), "rb") as fp:
+        with pd.read_csv(
+            fp,
+            sep="\t",
+            usecols=["tconst", "parentTconst", "seasonNumber", "episodeNumber"],
+            na_values="\\N",
+            chunksize=1000
+        ) as reader:
+            for chunk in reader:
+                
+
