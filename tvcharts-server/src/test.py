6e77db7614f7b45f56ffbdd9077a7c56bdc00ab5
Gus Beringer <gusberinger@gmail.com>
2022-07-24 21:43:55 +0100

    added search test

--- tvcharts-server/src/test.py
+++ tvcharts-server/src/test.py
@@ -1,5 +1,4 @@
-from tqdm import tqdm
-from server import app, search
+from server import app
 import unittest
 
 class FlaskTestCase(unittest.TestCase):
@@ -21,7 +20,16 @@ class FlaskTestCase(unittest.TestCase):
         response = tester.get("/poster/tt0141842", content_type = "image/jpeg")
         self.assertEqual(response.status_code, 200)
 
+    # tt0025509 has no votes on each episode
+    # make sure not in search database
+    def test_search(self):
+        tester = app.test_client(self)
+        response = tester.get("/search/", content_type = "html/text")
+        self.assertEqual(response.status_code, 200)
+        search_db = response.get_json()
+        tconst_list = [entry["tconst"] for entry in search_db]
+        self.assertNotIn("tt0025509", tconst_list)
+
 
 if __name__ == "__main__":
-    print(search)
     unittest.main()
