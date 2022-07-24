55d4a3fd6779c0a2c32716d28631feb57e190d34
Gus Beringer <gusberinger@gmail.com>
2022-07-24 21:17:33 +0100

    implement server api

--- tvcharts-client/src/MovieChart.js
+++ tvcharts-client/src/MovieChart.js
@@ -6,31 +6,13 @@ const MovieChart = props => {
   const [voteRows, setVoteRows] = useState([[0,0, "", "color: #fff" ]])
   const [ratingRows, setRatingRows] = useState([[0,0, "", "color: #fff" ]])
   const [title, setTitle] = useState("")
-  const colors = ["#8dd3c7", "#bebada", "#fb8072", "#80b1d3", "#fdb462", "#b3de69", "#fccde5", "#d9d9d9"]
   const [episodeCount, setEpisodeCount] = useState(200)
 
   const parseJson = data => {
-    let ratingRows = []
-    let voteRows = []
-    let episodeIndex = 0
     setTitle(data["title"])
-    const episode_info = data["episode_info"]
-    for (const seasonNumber of Object.keys(episode_info)) {
-        const seasonColor = `color: ${colors[(seasonNumber -1 ) % colors.length]}`
-        for (const episodeNumber of Object.keys(episode_info[seasonNumber])) {
-          const episode = episode_info[seasonNumber][episodeNumber]
-          episodeIndex++
-          const rating = episode['rating']
-          const votes = episode['votes']
-          const episodeTitle = episode['episodeTitle']
-          const annotation = `<b>Season ${seasonNumber} Epsiode ${episodeNumber}</b><br/>${episodeTitle}<br/>${votes.toLocaleString('en-US')} Votes<br/>${rating} Average Rating`
-          ratingRows.push([episodeIndex, rating, annotation, seasonColor])
-          voteRows.push([episodeIndex, votes, annotation, seasonColor])
-        }
-      }
-    setRatingRows(ratingRows)
-    setVoteRows(voteRows)
-    setEpisodeCount(episodeIndex)
+    setVoteRows(data["numVotes"])
+    setRatingRows(data["averageRating"])
+    setEpisodeCount(data["numVotes"].length)
   }
 
   useEffect(() => {
