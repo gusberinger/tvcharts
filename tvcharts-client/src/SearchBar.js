81a46eaf4996f2145f2157e2ee3a03431a40a9bc
Gus Beringer <gusberinger@gmail.com>
2022-07-25 00:51:43 +0100

    fuzzy search implemented

--- tvcharts-client/src/SearchBar.js
+++ tvcharts-client/src/SearchBar.js
@@ -1,29 +1,30 @@
 import React, {useState} from 'react'
 import './SearchBar.css'
-
+import Fuse from 'fuse.js'
 
 const SearchBar = ({placeholder, data, selectItem}) => {
   const [filteredData, setFilteredData] = useState([])
-  const [word, setWord] = useState("")
+  const [searchTerm, setSearchTerm] = useState("")
+  const fuse = new Fuse(data, {includeScore: true, keys: ["title"]})
 
   const handleChange = (e) => {
-    const searched = e.target.value
-    setWord(searched)
-    if (word !== "") {
-      const newFilter = data.filter((value) => {
-        return value.title.toLowerCase().includes(word.toLowerCase())
-      })
+    const newSearchTerm = e.target.value
+    if (searchTerm !== "") {
+      const results = fuse.search(newSearchTerm)
+      const newFilter = results.filter(item => item.score < .3)
       setFilteredData(newFilter)
     }
     else {
       setFilteredData([])
     }
+    setSearchTerm(newSearchTerm)
   }
+
 
   const handleSelect = e => {
     e.preventDefault()
     setFilteredData([])
-    setWord("")
+    setSearchTerm("")
     selectItem(e)
   }
 
@@ -34,21 +35,22 @@ const SearchBar = ({placeholder, data, selectItem}) => {
           type="text"
           placeholder={placeholder}
           onChange={handleChange}
-          value={word}
+          value={searchTerm}
         />
       </div>
       <div className="dataResult">
-        {filteredData.slice(0, 15).map((value, key) => {
+        {filteredData.slice(0, 8).map((value, index) => {
+          // console.log(value, index)
           return (
           <a
             className="dataItem"
             target="_blank"
             onClick={handleSelect}
-            id={value.id}
-            key={key}
+            // id={value.id}
+            key={index}
             href="# "
           >
-            <p tconst={value.tconst}>{value.title} ({value.startYear}–{value.endYear})</p>
+            <p tconst={value.item.tconst}>{value.item.title} ({value.item.startYear}–{value.item.endYear})</p>
           </a>
           )
         })}
