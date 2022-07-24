77b27c3edd60f1b6c2d67e3a1e221673a2a7a1d1
Gus Beringer <gusberinger@gmail.com>
2022-07-24 21:31:34 +0100

    re wrote testing  script

--- tvcharts-server/src/test.py
+++ tvcharts-server/src/test.py
@@ -6,15 +6,20 @@ class FlaskTestCase(unittest.TestCase):
 
     def test_sopranos(self):
         tester = app.test_client(self)
-        response = tester.get("/tconst/tt0141842", content_type = "html/text")
+        response = tester.get("/episodes/tt0141842", content_type = "html/text")
         self.assertEqual(response.status_code, 200)
 
-    def test_all(self):
-        tconsts = [x[0] for x in search] 
-        for tconst in tqdm(tconsts):
-            tester = app.test_client(self)
-            response = tester.get(f"/tconst/{tconst}", content_type = "html/text")
-            self.assertEqual(response.status_code, 200)
+    def test_specials(self):
+        # tt0383126 - mythbusters
+        # make sure specials with no season are not included
+        tester = app.test_client(self)
+        response = tester.get("/episodes/tt0383126", content_type = "html/text")
+        self.assertEqual(response.status_code, 200)
+
+    def test_poster_sopranos(self):
+        tester = app.test_client(self)
+        response = tester.get("/poster/tt0141842", content_type = "image/jpeg")
+        self.assertEqual(response.status_code, 200)
 
 
 if __name__ == "__main__":
